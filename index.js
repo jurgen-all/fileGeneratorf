b327e89149c0fccb79ea0940e43bbd5f6e0dbf9a
jurgen-all <119310598+jurgen-all@users.noreply.github.com>
2022-11-28 16:47:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // import code here
+import fs from 'fs';
+
+
+function parseCsv(file) {
+    const lines = file.split("\r\n");
+    for (let i = 0; i < lines.length; i++) {
+        lines[i] = lines[i].split(",");
+    }
+
+    return lines;
+
+}
+
+function writeFiles(array) {
+    for (let i = 0; i < array.length; i++) {
+        const fileName = array[i][0];
+        const data = array[i][1];
+        fs.writeFile(fileName, data, (err) => {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+    }
+}
+
+function createFiles(dataFile) {
+
+    fs.readFile(dataFile, 'utf-8', (err, val) => {
+        if (err) {
+            console.log(error);
+        }
+        else {
+            const arr = parseCsv(val);
+            writeFiles(arr);
+        }
+    });
+}
+
+
+createFiles('data.csv');
